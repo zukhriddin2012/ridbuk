7aec7243bdee12c0178310445e8297587eb2189c
zukhriddin2012 <zuhriddin2012@gmail.com>
2018-10-14 01:06:41 +0500

    commit

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
     TOKEN: '636297823:AAHcPiYoTAI_mJg_r4Jzdtzy-Jg2wEWcYe8',
-    DB_URL: 'mongodb://localhost/ridbuk_bot',
+    DB_URL: 'mongodb://zuhriddin:zukko1998@ds121212.mlab.com:21212/ridbuk',
     PAY: '371317599:TEST:283577343'
 }
